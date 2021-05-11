4663e4ceeebcd80f8b44296549835f376b6fe7d3
LAPTOP-0SQ3UHT2\as <henry.nguyen@contemi.com>
2021-05-11 13:59:48 +0700

    create wallet and transfer coin

--- Source/BTCN_Blockchain/BTCN_Blockchain/Controllers/HomeController.cs
+++ Source/BTCN_Blockchain/BTCN_Blockchain/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BTCN_Blockchain.Models;
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +10,31 @@ namespace BTCN_Blockchain.Controllers
 {
     public class HomeController : Controller
     {
-        
+
+        const string minerAddress = "miner1";
+        const string adminAddress = "admin";
+        const string user1Address = "user1";
+        const string user2Address = "user2";
+
+        // dùng tạm các biến static này để chứa dữ liệu
+        // không dùng static thì mỗi khi gọi actionResult biến blockChain sẽ bị reset về chain rỗng (1 block gốc)
+        private static BlockChain blockChain = new BlockChain(proofOfWorkDifficulty: 2, miningReward: 10);
         private static bool isLoaded = false; // chưa load -> nếu bằng true thì không load lại
+        private static List<string> walletList = new List<string>() { adminAddress, user1Address, user2Address }; // khởi đầu với 3 ví
         private static List<string> passwordList = new List<string>() { "admin", "user1", "user2" };
         private static string acc = ""; // tên ví, rỗng là chưa đăng nhập
 
         // Load , lúc đầu block chỉ có 1 khối, thêm 1 khối giao dịch (cho user1 và user2)
         public void Load()
         {
-           
+            blockChain.CreateTransaction(new Transactions(adminAddress, user1Address, 200));
+            blockChain.CreateTransaction(new Transactions(adminAddress, user2Address, 200));
+            blockChain.MineBlock(minerAddress);
+            blockChain.CreateTransaction(new Transactions(adminAddress, user1Address, 20));
+            blockChain.CreateTransaction(new Transactions(adminAddress, user2Address, 130));
+            blockChain.MineBlock(minerAddress);
         }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -31,7 +48,6 @@ namespace BTCN_Blockchain.Controllers
 
             return View();
         }
-
         public ActionResult Index()
         {
             // Index sẽ là nơi vào đầu tiên
@@ -41,7 +57,7 @@ namespace BTCN_Blockchain.Controllers
                 isLoaded = true;
             }
 
-           
+            ViewBag.AllChainContent = blockChain.GetHomeInfor();
 
             return View("Index");
         }
@@ -52,6 +68,29 @@ namespace BTCN_Blockchain.Controllers
             ViewBag.Acc = acc;
 
             return View("CreateWallet");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckCreateWallet(User _user)
+        {
+            if (walletList.Contains(_user.username))
+            {
+                ViewBag.status = "Tên đăng nhập đã tồn tại!";
+                ViewBag.Acc = acc;
+
+                return View("CreateWallet");
+            }
+            else
+            {
+                walletList.Add(_user.username);
+                passwordList.Add(_user.password);
+                acc = _user.username;
+
+                ViewBag.AllChainContent = blockChain.GetHomeInfor();
+
+                return View("Index");
+            }
         }
     }
 }
